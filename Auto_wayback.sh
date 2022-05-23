edd40e6701884ec161054d028855b87ced512464
sarden016 <56448040+sarden016@users.noreply.github.com>
2022-05-23 21:36:42 +0530

    Add files via upload

--- Auto_wayback.sh
+++ Auto_wayback.sh
@@ -17,13 +17,13 @@ fi
 
  
 echo "[+] Harvesting subdomain with subfinder"
-subfinder -d $url >> $url/recon/asset.txt
-cat $url/subfinder/alive.txt | grep $1 >> $url/recon/Final.txt
+subfinder -d $url >> $url/subfinder/asset.txt
+cat $url/subfinder/alive.txt 
 
 echo "[+] Probing for alive domains..."
-cat $url/recon/Final.txt | sort -u | httpx | tee $url/recon/httpx.txt
-sort -u $url/recon/httprobe/a.txt > $url/recon/httprobe/alive.txt
-rm $url/recon/httprobe/a.txt
+cat $url/subfinder/Final.txt | sort -u | httpx | tee $url/subfinder/httpx.txt
+sort -u $url/subfinder/httprobe/a.txt > $url/subfinder/httprobe/alive.txt
+rm $url/subfinder/httprobe/a.txt
 
 echo "[+] Scanning for open ports..."
-cat $url/recon/httprobe/alive.txt | waybackurls | tee $url/recon/httprobe/wayback.txt
+cat $url/subfinder/httprobe/alive.txt | waybackurls | tee $url/subfinder/httprobe/wayback.txt
