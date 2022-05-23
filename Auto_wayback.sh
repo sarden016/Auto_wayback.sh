d7befcdc17b0ff8a1981e2b9b31932cacd79acbd
sarden016 <56448040+sarden016@users.noreply.github.com>
2022-05-23 21:56:57 +0530

    Add files via upload

--- Auto_wayback.sh
+++ Auto_wayback.sh
@@ -6,24 +6,28 @@ if [ ! -d "url" ];then
 	mkdir $url
 fi
 
-if [ ! -d "$url/subfinder" ];then
-	mkdir $url/subfinder
+if [ ! -d "$url/subdomain" ];then
+	mkdir $url/subdomain
+fi
+
+if [ ! -d "$url/subdomain/httpx" ];then
+    mkdir $url/subdomain/httpx
 fi
 
 
-if [ ! -f "$url/subfinder/alive.txt" ];then
-    touch $url/subfinder/alive.txt
+if [ ! -f "$url/subdomain/alive.txt" ];then
+    touch $url/subdomain/alive.txt
 fi
 
  
-echo "[+] Harvesting subdomain with subfinder"
-subfinder -d $url >> $url/subfinder/asset.txt
-cat $url/subfinder/alive.txt 
+echo "[+] Harvesting subdomain with subdomain"
+subdomain -d $url >> $url/subdomain/asset.txt
+cat $url/subdomain/alive.txt 
 
 echo "[+] Probing for alive domains..."
-cat $url/subfinder/Final.txt | sort -u | httpx | tee $url/subfinder/httpx.txt
-sort -u $url/subfinder/httpx/a.txt > $url/subfinder/httpx/alive.txt
-rm $url/subfinder/httpx/a.txt
+cat $url/subdomain/Final.txt | sort -u | httpx | tee $url/subdomain/httpx.txt
+sort -u $url/subdomain/httpx/a.txt > $url/subdomain/httpx/alive.txt
+rm $url/subdomain/httpx/a.txt
 
 echo "[+] Scanning for open ports..."
-cat $url/subfinder/httpx/alive.txt | waybackurls | tee $url/subfinder/httpx/wayback.txt
+cat $url/subdomain/httpx/alive.txt | waybackurls | tee $url/subdomain/httpx/wayback.txt
